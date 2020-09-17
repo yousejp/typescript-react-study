2be4b0d599b0babf4efc1353c2363cc4e07b0e59
yousejp <yousejp25@gmail.com>
2020-09-18 04:01:30 +0900

    useRefのより実践的な使い方

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Counter: React.FunctionComponent = () => {
   // const Counter: React.FC = () => { FCはFunctionComponentと同じ機能
@@ -14,11 +14,34 @@ const Counter: React.FunctionComponent = () => {
     setValue((prevState) => prevState - 1);
   };
 
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    console.log('render was done');
+    renderTimes.current++;
+  });
+  const ref = useRef<HTMLInputElement>(null!);
+  // !をつけるとその型をつけるとその型ではないことを明示的にできる
+  // Non-Null Assertion Operatorという
+  const focusInput = () => {
+    // const current = ref.current;
+    // if (current != null) {
+    //   current.focus();
+    // }
+
+    ref.current?.focus();
+    // null出なければ実行される
+
+    ref.current.focus();
+  };
+
   return (
     <div>
       <div>Value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <div>This component was re-rendered {renderTimes.current} times!</div>
+      <input ref={ref} type="text" />
+      <button onClick={focusInput}>Click Me!</button>
     </div>
   );
 };
